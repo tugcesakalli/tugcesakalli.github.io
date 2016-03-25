44df6c4b3b300db3d1a70888ba6915c5459d40e6
tugcesakalli <tugcesakalli35@gmail.com>
2016-03-25 10:26:32 +0200

    25.03.2016

---
+++ students.js
@@ -0,0 +1,27 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var Student=[
+    'ID':'',
+    'fname':'',
+    'lname':'',
+    'courses':'',
+[
+    {'courseID':'',
+     'grade':''}
+]
+    calcGPA:function()[{
+            var gpa=0;
+            for(var i=0; i<this.courses.lengrth; i++){
+                gpa +=this.courses[i]/=this.courses.grade
+                gpa/=this.course.length;
+              return gpa;
+            }
+        };
+        var s1=student;
+        s1['ID']="200104018"
+        s1['fname']="first"
+        s1['lname']='last'
+        for (var i=0; i<S;  i++)
